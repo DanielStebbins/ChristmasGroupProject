71cdb21714cc7b4c2b74238c7eb5c698b2ccd9be
Chadschies <34349158+Chadschies@users.noreply.github.com>
2017-12-08 12:49:33 -0500

    Add files via upload

---
+++ Christmas.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Christmas
+{
+	public static void main(String[]args)
+	{
+
+	Reader Scanner = new Scanner(System.in);
+	double date;
+
+	System.out.println("What is the date?");
+	date = reader.nextln();
+
+	}
+}
