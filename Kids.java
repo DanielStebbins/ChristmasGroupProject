a360ede04cbe97c8e0009708e1cda7d69f4afd73
LukeBowman04 <34348826+LukeBowman04@users.noreply.github.com>
2017-12-11 12:00:40 -0500

    Add files via upload
    kids class

---
+++ Kids.java
@@ -0,0 +1,24 @@
+Public class Kids
+{
+	String name = "";
+	String behavior = "";
+	int age = 0;
+
+	public GetName()
+	{
+		name = ;
+		return name;
+	}
+
+	public GetBehavior()
+	{
+		behavior = ;
+		return behavior;
+	}
+
+	public Age()
+	{
+		age = ;
+		return age;
+	}
+}
