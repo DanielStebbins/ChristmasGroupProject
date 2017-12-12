acf2d10990799dad3ce80cf3653d2bd218ea3de4
DSWhiteHat <33768979+DSWhiteHat@users.noreply.github.com>
2017-12-11 22:12:02 -0500

    Added day calc. to file reading main

--- Code/Christmas.java
+++ Code/Christmas.java
@@ -19,5 +19,29 @@ public class Christmas
 			String name = fileReader.nextLine();
 			Gift  = new Gift(name, fileReader.nextInt(), fileReader.nextInt(), fileReader.nextInt(), fileReader.nextInt());
 		}
+		
+		System.out.println("What is the date? (ex. December 4th == 12.04)");
+ +		date = reader.nextDouble();
+ +
+ +		for(date = date + 0.00; date < 12.24; days++)
+ +		{
+ +			if(date == 1.31 || date == 3.31 || date == 5.31 || date == 7.31 || date == 8.31 || date == 10.31  || date == 12.31)
+ +			{
+ +				date = date + .69;
+ +			}
+ +
+ +			if(date == 2.28 )
+ +			{
+ +				date = date + .82;
+ +			}
+ +
+ +			if(date == 4.30 || date == 6.30 || date == 9.30 || date == 11.30)
+ +			{
+ +				date = date + .70;
+ +			}
+ +			date = date + .01;
+ +		}
+ +
+ +		System.out.println("There are " + days + " till Christmas");
  	}
 }
