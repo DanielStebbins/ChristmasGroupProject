05781f090185a34714f839a8e077c33c0d4455a6
DSWhiteHat <33768979+DSWhiteHat@users.noreply.github.com>
2017-12-12 11:39:12 -0500

    Add files via upload

--- Code/Christmas.java
+++ Code/Christmas.java
@@ -18,31 +18,31 @@ public class Christmas
 		while(fileReader.hasNextLine())
 		{
 			String name = fileReader.nextLine();
-			Gift  = new Gift(name, fileReader.nextInt(), fileReader.nextInt(), fileReader.nextInt(), fileReader.nextInt());
+			gifts.add(new Gift(fileReader.nextLine(), fileReader.nextInt(), fileReader.nextInt(), fileReader.nextInt(), fileReader.nextDouble()));
 		}
-		
+
 		System.out.println("What is the date? (ex. December 4th == 12.04)");
- +		date = reader.nextDouble();
- +
- +		for(date = date + 0.00; date < 12.24; days++)
- +		{
- +			if(date == 1.31 || date == 3.31 || date == 5.31 || date == 7.31 || date == 8.31 || date == 10.31  || date == 12.31)
- +			{
- +				date = date + .69;
- +			}
- +
- +			if(date == 2.28 )
- +			{
- +				date = date + .82;
- +			}
- +
- +			if(date == 4.30 || date == 6.30 || date == 9.30 || date == 11.30)
- +			{
- +				date = date + .70;
- +			}
- +			date = date + .01;
- +		}
- +
- +		System.out.println("There are " + days + " till Christmas");
+ 		date = reader.nextDouble();
+
+ 		for(date = date + 0.00; date < 12.24; days++)
+ 		{
+ 			if(date == 1.31 || date == 3.31 || date == 5.31 || date == 7.31 || date == 8.31 || date == 10.31  || date == 12.31)
+ 			{
+ 				date = date + .69;
+ 			}
+
+ 			if(date == 2.28 )
+ 			{
+ 				date = date + .82;
+ 			}
+
+ 			if(date == 4.30 || date == 6.30 || date == 9.30 || date == 11.30)
+ 			{
+ 				date = date + .70;
+ 			}
+ 			date = date + .01;
+ 		}
+
+ 		System.out.println("There are " + days + " till Christmas");
  	}
 }
