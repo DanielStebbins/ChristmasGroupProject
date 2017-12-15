daea7792a7f7a7e5edabba170e956fb27f41a8d8
HosE <34349158+Chadschies@users.noreply.github.com>
2017-12-15 11:59:43 -0500

    Add files via upload

---
+++ Code/Test.java
@@ -0,0 +1,17 @@
+//random number generator that goes off the givin range that is acessable after the parameters are met
+import java.util.Random;
+
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		int n;
+		Random rand = new Random();
+
+		n = rand.nextInt(currentGifts.size);
+
+
+
+	}
+}
